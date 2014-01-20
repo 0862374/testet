b98052f153df2d00800b052708987603607f9ddb
Nabil T. <0862374@hr.nl>
2014-01-20 14:29:42 +0100

    new test

--- src/main/java/com/test/testproj/App.java
+++ src/main/java/com/test/testproj/App.java
@@ -1,13 +1,21 @@
 package com.test.testproj;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		 
+		System.out.println(getHelloWorld());
+ 
+	}
+ 
+	public static String getHelloWorld() {
+ 
+		return "Hello World!";
+ 
+	}
+ 
+	public static String getHelloNetherlands() {
+ 
+		return "Hello Netherlands!";
+ 
+	}
 }
--- src/test/java/com/test/testproj/AppTest.java
+++ src/test/java/com/test/testproj/AppTest.java
@@ -1,38 +1,19 @@
 package com.test.testproj;
 
-import junit.framework.Test;
+import junit.framework.Assert;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public void testPrintHelloWorld() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		Assert.assertEquals(App.getHelloWorld(), "Hello World!");
+
+	}
+
+	public void testPrintHelloNetherlands() {
+
+		Assert.assertEquals(App.getHelloNetherlands(), "Hello Netherlands!");
+
+	}
 }
